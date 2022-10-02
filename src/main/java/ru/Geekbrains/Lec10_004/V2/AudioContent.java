4c0fcec0c4c7a7ac91d5b01fde61acb0eed8f256
Dmitriy_Klaes <89119584059@mail.ru>
2022-10-03 00:39:47 +0300

    Лекция от 22-10-01

---
+++ src/main/java/ru/Geekbrains/Lec10_004/V2/AudioContent.java
@@ -0,0 +1,7 @@
+package ru.Geekbrains.Lec10_004.V2;
+
+public class AudioContent extends Content {
+    public AudioContent(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/ru/Geekbrains/Lec10_004/V2/VideoContent.java
@@ -0,0 +1,7 @@
+package ru.Geekbrains.Lec10_004.V2;
+
+public class VideoContent extends Content {
+    public VideoContent(String name) {
+        super(name);
+    }
+}
